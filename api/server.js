7a62472d99634533c95b1fbf9e395e790a3e131c
Justin <jdlow2@gmail.com>
2019-02-01 12:22:32 -0500

    GET by id route and test

--- api/server.js
+++ api/server.js
@@ -3,8 +3,11 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
+let id = 2;
+
 const games = [
   {
+    id: 1,
     title: 'Test Game',
     genre: 'Action',
     releaseYear: 2019,
@@ -18,15 +21,29 @@ server.get('/games', (req, res) => {
   res.status(200).send(games);
 });
 
+server.get('/games/:id', (req, res) => {
+  const game = games.filter(g => g.id == req.params.id)[0];
+  console.log(game);
+  
+  
+  
+  if (!game) {
+    res.status(404).json({error: 'invalid id'})
+  } else {
+    res.status(200).json(game);
+  }
+})
+
 server.post('/games', (req, res) => {
   const { title, genre, releaseYear } = req.body;
 
   if (!title || !genre) {
     res.status(422).send({ error: 'Game must have a title and release year!' });
   } else {
-    games.push(req.body);
+    games.push({id: id++, ...req.body});
     res.status(201).send({ message: 'Game created' });
   }
 });
+
 
 module.exports = server;
--- api/server.test.js
+++ api/server.test.js
@@ -13,11 +13,17 @@ describe('/ sanity check', () => {
 describe('/POST endpoint', () => {
   it('should return an error if the object shape is invalid', async () => {
     try {
-      const body = {
+      let body = {
         title: 'Contra',
         releaseYear: 1986,
       };
       const response = await request(server).post('/games').send(body);
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({ error: 'Game must have a title and release year!' });
+      body = {
+        releaseYear: 1986,
+        genre: 'Fantasy',
+      };
       expect(response.status).toBe(422);
       expect(response.body).toEqual({ error: 'Game must have a title and release year!' });
     } catch(err) {
@@ -53,12 +59,22 @@ describe('/GET endpoint', () => {
   it('should return an array', async () => {
     try {
     const response = await request(server).get('/games');
-      console.log(response.body);
-      
-      expect(response.body).toEqual(expect.arrayContaining());
+      expect(response.body).toEqual(expect.arrayContaining([]));
       
     } catch(err) {
       console.log(err);
     }
+  })
+})
+
+describe('/GET/:id endpoint', () => {
+  it('should return a 404 if the game does not exist', async () => {
+    const response = await request(server).get('/games/12');
+    expect(response.status).toBe(404);
+    expect(response.body).toEqual({error: 'invalid id'})
+  })
+  it('should return the game if the id is valid', async () => {
+    const response = await request(server).get('/games/1');
+    expect(response.status).toBe(200);
   })
 })
